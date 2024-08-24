a0523a9586a58a02be9e04c042b58a06eb35f56f
Ohmaigodi <150306279+Ohmaigodi@users.noreply.github.com>
2024-08-23 22:17:07 -0300

    migodi

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function visibilidade() {
+  var visi = document.querySelector(".DIV2")
+  visi.style.visibility= ("inherit");
+}
+function visibilidade2() {
+  var visi = document.querySelector(".DIV2")
+  visi.style.visibility= ("hidden");
+}
+function vrsmc() {
+  var vrs = document.querySelector(".vrsmc")
+  vrs.style.visibility=("inherit");
+}
